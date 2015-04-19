c0ea82d9ddb19255a7d75d801d4c64cbbbbd88a7
Max Eggers <eggers.max@gmail.com>
2015-04-19 17:46:37 -0500

    flask boilerplate

---
+++ server/__init__.py
@@ -0,0 +1,14 @@
+import os
+import json
+from flask import Flask, request, Response
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object('server.settings')
+db = SQLAlchemy(app)
+session = db.session
+app.url_map.strict_slashes = False
+
+import server.models
+import server.controllers
---
+++ server/settings.py
@@ -0,0 +1,4 @@
+DEBUG = True
+
+SECRET_KEY = 'temporary_secret_key'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///server.db'
