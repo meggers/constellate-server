9c771281d87a377dbdb4da6e1006d75bf276ebe3
Max Eggers <eggers.max@gmail.com>
2015-04-20 13:55:48 -0500

    defined user api

---
+++ server/controllers.py
@@ -0,0 +1,37 @@
+from server import app, db, session
+from flask import Flask, request, Response
+
+# routing for basic page #
+@app.route('/')
+def basic_pages(**kwargs):
+    return redirect('/static/index.html')
+
+
+#******* API v1 ********#
+
+# api authentication #
+@app.route('/api/v1/login', methods=['GET'])
+def login():
+    return {'':''}, 200
+
+@app.route('/api/v1/logout', methods=['GET'])
+def logout():
+    return {'':''}, 200
+
+# user #
+@app.route('/api/v1/user/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    return {'':''}, 200
+
+@app.route('/api/v1/user/<int:user_id>', methods=['PUT'])
+def update_user():
+    return {'':''}, 200
+
+@app.route('/api/v1/user/<int:user_id>', methods=['POST'])
+def add_user():
+    return {'':''}, 200
+
+@app.route('/api/v1/user/<int:user_id>', methods=['DELETE'])
+def delete_user():
+    return {'':''}, 200
+
