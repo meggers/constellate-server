2676d4e890580bf4ea7c8dd90455db483a3620b0
Max Eggers <eggers.max@gmail.com>
2015-04-20 14:10:19 -0500

    removed unneeded req and add constellation api

--- server/controllers.py
+++ server/controllers.py
@@ -19,19 +19,44 @@ def logout():
     return {'':''}, 200
 
 # user #
+@app.route('/api/v1/user/', methods=['POST'])
+def add_user():
+    return {'':''}, 200
+
 @app.route('/api/v1/user/<int:user_id>', methods=['GET'])
 def get_user(user_id):
     return {'':''}, 200
 
 @app.route('/api/v1/user/<int:user_id>', methods=['PUT'])
-def update_user():
-    return {'':''}, 200
-
-@app.route('/api/v1/user/<int:user_id>', methods=['POST'])
-def add_user():
+def update_user(user_id):
     return {'':''}, 200
 
 @app.route('/api/v1/user/<int:user_id>', methods=['DELETE'])
-def delete_user():
+def delete_user(user_id):
+    return {'':''}, 200
+
+# constellations #
+@app.route('/api/v1/constellations/', methods=['POST'])
+def add_constellation(constellation_id):
+    return {'':''}, 200
+
+@app.route('/api/v1/constellations/', methods=['GET'])
+def get_constellations():
+    return {'':''}, 200
+
+@app.route('/api/v1/constellations/<int:constellation_id>', methods=['GET'])
+def get_constellation(constellation_id):
+    return {'':''}, 200
+
+@app.route('/api/v1/constellations/<int:constellation_id>', methods=['PUT'])
+def update_constellation(constellation_id):
+    return {'':''}, 200
+
+@app.route('/api/v1/constellations/star/<int:star_id>', methods=['GET'])
+def get_constellations_with_star(star_id):
+    return {'':''}, 200
+
+@app.route('/api/v1/constellations/<int:constellation_id>', methods=['DELETE'])
+def delete_constellation(constellation_id):
     return {'':''}, 200
 
