2be4a8cf415e8aa5089f2ca65748bd273dca8b7a
Max Eggers <eggers.max@gmail.com>
2015-04-20 11:30:25 -0500

    added token generation

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from server import app, db, session
 from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,6 +21,24 @@ class User(db.Model):
 
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
+
+    def generate_token(self, expiration = 600):
+        s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
+        return s.dumps({ 'id': self.id })
+
+    @staticmethod
+    def verify_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None # valid token, but expired
+        except BadSignature:
+            return None # invalid token
+
+        user = User.query.get(data['id'])
+        return user
 
 class Star(db.Model):
     id = db.Column(db.Integer, primary_key=True)
