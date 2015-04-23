bf533b9479ca2b267142aff35496627f632cb450
Max Eggers <eggers.max@gmail.com>
2015-04-22 19:51:50 -0500

    add get constellation test

--- api_test.sh
+++ api_test.sh
@@ -9,20 +9,30 @@ CONSTELLATION='{
   ]
 }'
 
+echo "login test"
 JSON=$(curl -su default:default -X GET http://127.0.0.1:5000/api/v1/login)
+printf "response:\n$JSON\n\n"
 
 TOKEN=$(echo $JSON | jq '.token')
 TOKEN="${TOKEN%\"}"
 TOKEN="${TOKEN#\"}"
 
 echo "GET user test"
-curl -su ${TOKEN}:default -X GET http://127.0.0.1:5000/api/v1/user
-printf "\n"
+JSON=$(curl -su ${TOKEN}:default -X GET http://127.0.0.1:5000/api/v1/user)
+printf "response:\n$JSON\n\n"
 
 echo "POST constellation test"
-curl -su ${TOKEN}:default -X POST -H "Content-Type: application/json" -d "${CONSTELLATION}" http://127.0.0.1:5000/api/v1/constellations
-printf "\n"
+JSON=$(curl -su ${TOKEN}:default -X POST -H "Content-Type: application/json" -d "${CONSTELLATION}" http://127.0.0.1:5000/api/v1/constellations)
+printf "response:\n$JSON\n\n"
+
+CONSTELLATION_ID=$(echo $JSON | jq '.constellation_id')
+CONSTELLATION_ID="${CONSTELLATION_ID%\"}"
+CONSTELLATION_ID="${CONSTELLATION_ID#\"}"
 
 echo "GET constellations test"
-curl -su ${TOKEN}:default -i -X GET http://127.0.0.1:5000/api/v1/constellations
-printf "\n"
+JSON=$(curl -su ${TOKEN}:default -X GET http://127.0.0.1:5000/api/v1/constellations)
+printf "response:\n$JSON\n\n"
+
+echo "GET constellation/id test"
+JSON=$(curl -su ${TOKEN}:default -X GET http://127.0.0.1:5000/api/v1/constellations/${CONSTELLATION_ID})
+printf "response:\n$JSON\n\n"
